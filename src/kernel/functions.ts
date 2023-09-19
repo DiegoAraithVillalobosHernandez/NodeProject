e11678c733cdaefe98fb593d5bfd4fd7f0738785
20203tn063@utez.edu.mx <=>
2023-09-19 17:22:03 -0600

    first commit

---
+++ src/kernel/functions.ts
@@ -0,0 +1,13 @@
+import bcryptjs from "bcryptjs";
+
+export async function hash(password: string) {
+  return new Promise((resolve, reject) => {
+    // ! indica que si viene un dato y es de ese tipo
+    bcryptjs.hash("B4c0//", process.env.BCRYPTJS!, (err, hash) => {
+      if (err) reject(err);
+      resolve(hash);
+    });
+  });
+}
+
+export async function compare(params) {}
